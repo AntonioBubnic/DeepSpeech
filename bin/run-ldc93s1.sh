edcab0e6474c1bbfae14f77d42269c907173c17b
Antonio Bubnic <unknown12@nomail.com>
2022-07-19 13:16:01 +0200

    test1

--- bin/run-ldc93s1.sh
+++ bin/run-ldc93s1.sh
@@ -7,13 +7,13 @@ fi;
 
 if [ ! -f "data/ldc93s1/ldc93s1.csv" ]; then
     echo "Downloading and preprocessing LDC93S1 example data, saving in ./data/ldc93s1."
-    #python -u bin/import_ldc93s1.py ./data/ldc93s1
+    python -u bin/import_ldc93s1.py ./data/ldc93s1
 fi;
 
 if [ -d "${COMPUTE_KEEP_DIR}" ]; then
     checkpoint_dir=$COMPUTE_KEEP_DIR
 else
-    checkpoint_dir=$(python -c 'from xdg import BaseDirectory as xdg; print(xdg.save_data_path("deepspeech/ldc93s1"))')
+    checkpoint_dir=$(python -c 'from xdg import BaseDirectory as xdg; print(xdg.save_data_path("deepspeech/data/test1"))')
 fi
 # Force only one visible device because we have a single-sample dataset
 # and when trying to run on multiple devices (like GPUs), this will break
@@ -25,6 +25,6 @@ python -u DeepSpeech.py --noshow_progressbar \
   --train_batch_size 1 \
   --test_batch_size 1 \
   --n_hidden 100 \
-  --epochs 200 \
-  --checkpoint_dir "$checkpoint_dir" \
-  "$@"
+  --epochs 100 \
+  #--checkpoint_dir "$checkpoint_dir" \
+  #"$@"

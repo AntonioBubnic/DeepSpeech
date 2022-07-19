4ae5a21a226c2ef2ff3c1c4bfaee3b2f8fa6b32c
Antonio Bubnic <unknown12@nomail.com>
2022-07-19 13:46:06 +0200

    test1

--- bin/run-ldc93s1.sh
+++ bin/run-ldc93s1.sh
@@ -6,7 +6,8 @@ if [ ! -f DeepSpeech.py ]; then
 fi;
 
 if [ ! -f "data/test1/test_excel.csv" ]; then
-    echo "Downloading and preprocessing LDC93S1 example data, saving in ./data/ldc93s1."
+    echo "ne to Downloading and preprocessing LDC93S1 example data, saving in ./data/ldc93s1."
+    
     #python -u bin/import_ldc93s1.py ./data/ldc93s1
 fi;
 
@@ -20,8 +21,8 @@ fi
 export CUDA_VISIBLE_DEVICES=0
 
 python -u DeepSpeech.py --noshow_progressbar \
-  --train_files data/test1/test_excel.csv \
-  --test_files data/test1/test_excel.csv \
+  --train_files data/LDC93S1/test_excel.csv \
+  --test_files data/LDC93S1/test_excel.csv \
   --train_batch_size 1 \
   --test_batch_size 1 \
   --n_hidden 100 \

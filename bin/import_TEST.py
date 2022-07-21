00e321cf7088d5ded7cc07df9e8cd50fef32e1cf
Antonio Bubnic <unknown12@nomail.com>
2022-07-21 12:04:08 +0200

    new import try1

--- bin/import_TEST.py
+++ bin/import_TEST.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 import os
-import sys
 import pandas
-
-from deepspeech_training.util.downloader import maybe_download
+import sys
 
 df = pandas.DataFrame(
-    data=[(os.path.abspath('data/test'), os.path.getsize('data/test'), transcript)],
+    data=[(os.path.abspath("data/test/4ab96362a9e1e514860c8b19787051a8.wav"), os.path.getsize("data/test/test1_tekst.txt"), transcript)],
     columns=["wav_filename", "wav_filesize", "transcript"],
 )
-df.to_csv(os.path.join('data/test', "test_excel1.csv"), index=False)
+df.to_csv(os.path.join("data/test", "test_excel1.csv"), index=False)
+
+#df = pandas.DataFrame(
+#data=[(os.path.abspath(local_file), os.path.getsize(local_file), transcript)],
+#columns=["wav_filename", "wav_filesize", "transcript"],
+#)
+#df.to_csv(os.path.join(data_dir, "ldc93s1.csv"), index=False)#

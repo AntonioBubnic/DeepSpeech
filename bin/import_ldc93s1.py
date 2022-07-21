f4ba14b02edebdd01edc791db7c91ed4f2cd353a
Antonio Bubnic <unknown12@nomail.com>
2022-07-21 12:39:48 +0200

    new import try1

--- bin/import_ldc93s1.py
+++ bin/import_ldc93s1.py
@@ -5,7 +5,7 @@ import sys
 from deepspeech_training.util.downloader import maybe_download
 
 
-def _download_and_preprocess_data(data_dir):
+#def _download_and_preprocess_data(data_dir):
     # Conditionally download data
     #LDC93S1_BASE = "test"
     #LDC93S1_BASE_URL = "https://catalog.ldc.upenn.edu/desc/addenda/"
@@ -20,11 +20,11 @@ def _download_and_preprocess_data(data_dir):
     #        ".", ""
     #    )
 
-    df = pandas.DataFrame(
-        data=[(os.path.abspath("./data/test/4ab96362a9e1e514860c8b19787051a8.wav"), os.path.getsize("data/test/test1_tekst.txt"), transcript)],
-        columns=["wav_filename", "wav_filesize", "transcript"],
-    )
-    df.to_csv(os.path.join("./data/test", "test_excel1.csv"), index=False)
+#    df = pandas.DataFrame(
+#        data=[(os.path.abspath("./data/test/4ab96362a9e1e514860c8b19787051a8.wav"), os.path.getsize("./data/test/test1_tekst.txt"), transcript)],
+#        columns=["wav_filename", "wav_filesize", "transcript"],
+#    )
+#    df.to_csv(os.path.join("./data/test", "test_excel1.csv"), index=False)
 
 #df = pandas.DataFrame(
 #data=[(os.path.abspath(local_file), os.path.getsize(local_file), transcript)],
@@ -32,5 +32,11 @@ def _download_and_preprocess_data(data_dir):
 #)
 #df.to_csv(os.path.join(data_dir, "ldc93s1.csv"), index=False)#
 
-if __name__ == "__main__":
-    _download_and_preprocess_data(sys.argv[1])
+df = pandas.DataFrame(
+        data=[(os.path.abspath("./data/test/4ab96362a9e1e514860c8b19787051a8.wav"), os.path.getsize("./data/test/test1_tekst.txt"), "Upali ledicu")],
+        columns=["wav_filename", "wav_filesize", "transcript"],
+    )
+df.to_csv(os.path.join("./data/test", "test_excel1.csv"), index=False)
+
+#if __name__ == "__main__":
+#    _download_and_preprocess_data(sys.argv[1])
